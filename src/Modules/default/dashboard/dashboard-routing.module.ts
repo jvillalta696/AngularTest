26f01eba05cd3eb2d5d85491b448440a62643e3a
jvillalta696 <jvillalta696@gmail.com>
2022-03-06 19:06:26 -0600

    mejorando sidebar y links

--- src/Modules/default/dashboard/dashboard-routing.module.ts
+++ src/Modules/default/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from 'src/Modules/dashboard/about-us/about-us.component';
+import { ContactComponent } from 'src/Modules/dashboard/contact/contact.component';
 import { StartComponent } from 'src/Modules/dashboard/start/start.component';
 
 const routes: Routes = [
-  { path: 'start', component: StartComponent }
+  { path: 'start', component: StartComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
